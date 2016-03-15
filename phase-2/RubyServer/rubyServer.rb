169f536da48b9fcbd0de75adae716cfe84d5c70e
sebabelmar <si.belmar@gmail.com>
2016-03-15 15:13:29 -0700

    responding to client instructions

--- phase-2/RubyServer/rubyServer.rb
+++ phase-2/RubyServer/rubyServer.rb
@@ -3,11 +3,25 @@ require 'socket'
 server = TCPServer.new 2000
 
 loop do
+  # Server accepting comunication from client
   client = server.accept
-  client.puts "this is #{client.gets}"
+  resp = ''
 
-  puts "In the server..."
-  client.puts "YEAH!!"
+  # Client IO gets on flick
+  # Important to use chomp here because the string
+  # comes with /n "line break"
+  input = client.gets.chomp
+  case input
+    when "home"
+      resp = "Welcome home!"
+    when "profile"
+      resp = "Hey this is Seba."
+    else
+      resp = "You are out!"
+  end
+
+  client.puts resp
+  puts "In the server... responding to '#{input}'"
 
   client.close
 end

cb6d5d3429e2a5238125f389fddbf01ae20e4d18
sebabelmar <si.belmar@gmail.com>
2016-03-16 12:10:29 -0700

    HTTP... returning html

--- phase-2/RubyServer/reflextions.md
+++ phase-2/RubyServer/reflextions.md
@@ -1,4 +1,7 @@
 ## reflections
+I'll move from TCP to HTTP
+Understanding that this are two protocols sitting at 2 different layers in te Internet Protocol Suite
 
-* TCP Protocol
+HTTP on application layer and TCP as the transmition layer
 
+The suite has more lower level layers and those relates to comunication BTW Hardaware and other networks.
--- phase-2/RubyServer/rubyServer.rb
+++ phase-2/RubyServer/rubyServer.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require 'json'
 
 server = TCPServer.new 2000
 
@@ -10,18 +11,31 @@ loop do
   # Client IO gets on flick
   # Important to use chomp here because the string
   # comes with /n "new line"
-  input = client.gets.chomp
-  case input
-    when "home"
-      resp = "Welcome home!"
-    when "profile"
-      resp = "Hey this is Seba."
-    else
-      resp = "You are out!"
+  # input = client.gets.chomp
+  # case input
+  #   when "home"
+  #     resp = "Welcome home!"
+  #   when "profile"
+  #     resp = "Hey this is Seba."
+  #   else
+  #     resp = "You are out!"
+  # end
+
+  File.open('myHtml.html', "rb") do |file|
+    client.print "HTTP/1.1 200 OK\r\n" +
+    "Server : Seba's Server 1.0\r\n" +
+    "Content-Type: text/html\r\n" +
+    "Content-Length: #{file.size}\r\n" +
+    "Connection: close\r\n"
+
+    client.print "\r\n"
+
+    # write the contents of the file to the socket
+    IO.copy_stream(file, client)
   end
 
   client.puts resp
-  puts "In the server... responding to '#{input}'"
+  # puts "In the server... responding to '#{input}'"
 
   client.close
 end
